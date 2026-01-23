7759df32ddd1b1a5c48d1f0a77b87dc024ad4c12
damccull <401104+damccull@users.noreply.github.com>
2026-01-23 10:35:58 -0600

    Remove home and blog

--- ynab-payee-rs/src/main.rs
+++ ynab-payee-rs/src/main.rs
@@ -4,11 +4,12 @@ use dioxus::prelude::*;
 #[rustfmt::skip]
 enum Route {
     #[layout(Navbar)]
+    // #[route("/")]
+    // Home {},
+    // #[route("/blog/:id")]
+    // Blog { id: i32 },
     #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
-    #[route("/payees")]
+    // #[route("/payees")]
     Payees {},
     #[route("/transactions")]
     Transactions {},
@@ -32,31 +33,10 @@ fn App() -> Element {
     }
 }
 
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
-        }
-    }
-}
-
 /// Home page
 #[component]
 fn Home() -> Element {
-    rsx! {
-        Hero {}
-
-    }
+    rsx! {}
 }
 
 /// Payees Page
@@ -139,30 +119,30 @@ fn Transaction() -> Element {
     }
 }
 
-/// Blog page
-#[component]
-pub fn Blog(id: i32) -> Element {
-    rsx! {
-        div {
-            id: "blog",
-
-            // Content
-            h1 { "This is blog #{id}!" }
-            p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
-
-            // Navigation links
-            Link {
-                to: Route::Blog { id: id - 1 },
-                "Previous"
-            }
-            span { " <---> " }
-            Link {
-                to: Route::Blog { id: id + 1 },
-                "Next"
-            }
-        }
-    }
-}
+// /// Blog page
+// #[component]
+// pub fn Blog(id: i32) -> Element {
+//     rsx! {
+//         div {
+//             id: "blog",
+//
+//             // Content
+//             h1 { "This is blog #{id}!" }
+//             p { "In blog #{id}, we show how the Dioxus router works and how URL parameters can be passed as props to our route components." }
+//
+//             // Navigation links
+//             Link {
+//                 to: Route::Blog { id: id - 1 },
+//                 "Previous"
+//             }
+//             span { " <---> " }
+//             Link {
+//                 to: Route::Blog { id: id + 1 },
+//                 "Next"
+//             }
+//         }
+//     }
+// }
 
 /// Shared navbar component.
 #[component]
@@ -170,14 +150,14 @@ fn Navbar() -> Element {
     rsx! {
         div {
             id: "navbar",
-            Link {
-                to: Route::Home {},
-                "Home"
-            }
-            Link {
-                to: Route::Blog { id: 1 },
-                "Blog"
-            }
+            // Link {
+            //     to: Route::Home {},
+            //     "Home"
+            // }
+            // Link {
+            //     to: Route::Blog { id: 1 },
+            //     "Blog"
+            // }
             Link {
                 to: Route::Payees {},
                 "Payees"
